8bebcfa832ad0029c2044db35266ada2b8f1f130
jerrey moss <JerreyMoss@24dcaea7d349>
2018-05-18 15:45:38 +0000

    Switched to using Flask-Nav for the navigation bar.

--- flask_app.py
+++ flask_app.py
@@ -1,18 +1,31 @@
-
-from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from flask import render_template
-import constants
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from flask_nav import Nav
+from flask_nav.elements import Navbar, Subgroup, View
 
 app = Flask(__name__)
 app.config.from_object('config.BaseConfig')
 db = SQLAlchemy(app)
+Bootstrap(app)
+class Course(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    period = db.Column(db.Integer)
+    name = db.Column(db.String(80))
+    teacher_name = db.Column(db.String(80))
+    resource_name = db.Column(db.String(80))
+    resource_url = db.Column(db.String(300))
 
+class Song(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    artist_name = db.Column(db.String(80))
+    youtube_url = db.Column(db.String(300))
 
 @app.route('/about_me')
 def about_me():
-    return render_template('about_me.html',
-                            courses=constants.COURSES)
+    return render_template('about_me.html')
 '''
 @app.route('/class_schedule')
 def class_schedule():
@@ -21,19 +34,73 @@ def class_schedule():
 
 @app.route('/class_schedule')
 def class_schedule():
+    courses = Course.query.all()
     return render_template('class_schedule.html',
-                           courses=constants.COURSES)
+                           courses=courses)
 
 @app.route('/register')
 def register():
-    return render_template('register.html',
-                            courses=constants.COURSES)
+    return render_template('register.html')
 
 @app.route('/')
 def homepage():
     return render_template('index.html')
 
+
 @app.route('/top_ten_songs')
 def top_ten_songs():
+    songs = Song.query.all()
     return render_template('top_ten_songs.html',
-                           songs=constants.TOP_TEN_SONGS)
+                           songs=songs)
+
+nav = Nav(app)
+@nav.navigation('mysite_navbar')
+def create_navbar():
+    home_view = View('Home', 'homepage')
+    register_view = View('Register', 'register')
+    about_me_view = View('About Me', 'about_me')
+    class_schedule_view = View('Class Schedule', 'class_schedule')
+    top_ten_songs_view = View('Top Ten Songs', 'top_ten_songs')
+    misc_subgroup = Subgroup('Misc',
+                             about_me_view,
+                             class_schedule_view,
+                             top_ten_songs_view)
+    return Navbar('MySite', home_view, misc_subgroup, register_view)
+
+if __name__ == '__main__':
+  db.create_all()
+
+from flask_nav import Nav
+from flask_nav.elements import Navbar, Subgroup, View
+  # Add the lines below to flask_app.py
+nav = Nav(app)
+@nav.navigation('mysite_navbar')
+def create_navbar():
+    home_view = View('Home', 'homepage')
+    register_view = View('Register', 'register')
+    about_me_view = View('About Me', 'about_me')
+    class_schedule_view = View('Class Schedule', 'class_schedule')
+    top_ten_songs_view = View('Top Ten Songs', 'top_ten_songs')
+    misc_subgroup = Subgroup('Misc',
+                             about_me_view,
+                             class_schedule_view,
+                             top_ten_songs_view)
+    return Navbar('MySite', home_view, misc_subgroup, register_view)
+
+from werkzeug.security import check_password_hash
+from werkzeug.security import generate_password_hash
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15))
+    email = db.Column(db.String(150))
+    password_hash = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+from flask import redirect
+from flask import url_for
+from wtforms.validators import ValidationError
